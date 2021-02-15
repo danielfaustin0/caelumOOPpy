21dceee2620a9febb2589618fb235082e41d110f
Daniel Faustino <daniel.fsantos@protonmail.com>
2021-02-15 16:25:26 -0300

    The script is working.

--- games/guessing-game.py
+++ games/guessing-game.py
@@ -1,1 +1,16 @@
+"""Jogo de adivinhação
+
+O usuário deve descobrir o número secreto.
+
+    Feb 15 2021 - Daniel Faustino
+"""
+
 # TODO: define a constant to be the hidden number
+secret_number = 40
+
+attempt = int(input("Digite o seu palpite: "))
+
+if attempt == secret_number:
+    print('Você acertou.')
+else:
+    print('Você errou.')
