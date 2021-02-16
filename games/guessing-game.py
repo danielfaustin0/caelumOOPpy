92fe12bed0f8aa2b11fa16bcd3d4ccd949492946
Daniel Faustino <daniel.fsantos@protonmail.com>
2021-02-16 14:40:30 -0300

    Count how many time missed the number.

--- games/guessing-game.py
+++ games/guessing-game.py
@@ -2,6 +2,7 @@
 
 O usuário deve descobrir o número secreto.
 O programa continua sendo executado até que o usuário acerte o número.
+Quando o número for encontrado, será mostrado o total de tentativas.
 
     Feb 15 2021 - Daniel Faustino
 """
@@ -11,11 +12,13 @@ import os
 # TODO: generate random numbers
 secret_number = random.randint(1, 50) # generate random numbers
 
-while True:    
+failure = 0  # count how many times missed the number
+while True:
     attempt = int(input("Digite o seu palpite: "))
 
     if attempt == secret_number:
         print('Você acertou.')
+        print(f'Erros: {failure}')
         break
     else:
         print('Você errou.')
@@ -23,4 +26,4 @@ while True:
             print('O número secreto é maior do que seu palpite.')
         else:
             print('O número secreto menor do que seu palpite.')
-            
+        failure += 1
