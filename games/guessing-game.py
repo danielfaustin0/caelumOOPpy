90c8eff36ee37d5fcbb1ebf9ca054b7e438a711d
Daniel Faustino <daniel.fsantos@protonmail.com>
2021-02-17 09:40:09 -0300

    Create difficulty levels.

--- games/guessing-game.py
+++ games/guessing-game.py
@@ -4,23 +4,49 @@ O usuário deve descobrir o número secreto.
 O programa continua sendo executado até que o usuário acerte o número.
 Quando o número for encontrado, será mostrado o total de tentativas.
 
+O usuário pode escolher entre três níveis de dificuldade
+Em cada nível aumenta a escala de números e diminui as tentativas.
+
     Feb 15 2021 - Daniel Faustino
 """
 
-# TODO: generate random numbers
+# TODO: DONE generate random numbers
 # TODO: create difficulty levels
 # TODO: Create punctuation system
 # TODO: Stop game running
 # TODO: Verify user's input
 # TODO: Clear the game screen
+
 import random
 import os
 
 
-secret_number = random.randint(1, 50) # generate random numbers
-
 failure = 0  # count how many times missed the number
-while True:
+number_limit = 0
+
+while number_limit == 0:
+    level = int(input("""Selecione a dificuldade:
+    \nNível 1 \nNível 2 \nNível 3 \n:"""))
+
+    if level == 1:
+        print('Você tem 10 tentativas.\nNúmeros entre 01 e 30.')
+        number_limit = 30
+        failure = 10
+    elif level == 2:
+        print('Você tem 05 tentativas.\nNúmeros entre 01 e 45.')
+        number_limit = 45
+        failure = 9
+    elif level == 3:
+        print('Você tem 03 tentativas.\nNúmeros entre 01 e 60')
+        number_limit = 60
+        failure = 5
+    else:
+        print("Escolha a dificuldade correta.")
+
+
+secret_number = random.randint(1, number_limit) # generate random numbers
+
+while failure > 0:
     attempt = int(input("Digite o seu palpite: "))
 
     if attempt == secret_number:
@@ -33,4 +59,4 @@ while True:
             print('O número secreto é maior do que seu palpite.')
         else:
             print('O número secreto menor do que seu palpite.')
-        failure += 1
+        failure -= 1
