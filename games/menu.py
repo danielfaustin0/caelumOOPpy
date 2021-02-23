6b2e31f214b0b26285d12d3a77befc2bdbf228bb
Daniel Faustino <daniel.fsantos@protonmail.com>
2021-02-23 16:45:17 -0300

    Updating.

--- games/menu.py
+++ games/menu.py
@@ -7,5 +7,40 @@
 """
 
 import os
-import guessing_game
 import gallows_game
+import guessing_game
+
+
+def clear():
+    os.system('clear')
+
+leaving_game = False
+
+while not leaving_game:
+    print('Escolha um jogo:')
+    print('Jogo de adivinhação (1)')
+    print('Jogo da Forca (2)')
+    print('Aperte "q" para sair.')
+
+    game = input()
+    clear()
+
+    if game.isnumeric():
+        chosen_game = int(game)
+
+        if chosen_game == 1:
+
+            print('Bem vindo ao Jogo de Adivinhação.')
+            guessing_game.play_game()
+        elif chosen_game == 2:
+            print('Bem vindo ao Jogo Da Forca.')
+            gallows_game.play_game()
+        else:
+            print('Opção Inválida.')
+    else:
+        if game.isalpha():
+            if game == 'q':
+                print('Saindo do jogo.')
+                leaving_game = True
+            else:
+                print('Opção inválida.')
